4e0631501e43821a2a770a710f953bd9a66322fd
s1140320-web <s1140320@dmhs.kh.edu.tw>
2025-10-17 16:19:46 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <string>
+
+int main() {
+    int age =18;
+    double height=170.5;
+    std::string name ="lucas";
+    bool is_pass =true;
+    std::cout << "吳岳洧: "<<name<<"身高: "<<height<<std::endl;
+    std::cout << "年齡: "<<age<< "是否及格:" << is_pass << std::endl;
+    return 0;
+}
