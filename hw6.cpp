fd34eb47d22e1fa0fa42b4d9c80d1ef024ceab7f
s1140320-web <s1140320@dmhs.kh.edu.tw>
2025-11-21 16:04:30 +0800

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    cout<<"吳岳洧";
+    int age;
+    cin >> age;
+    if (age >= 18) {
+    cout << "18歲成年，可以看 pxxxhub 網站" >> endl;
+}
+    if (age < 18) {
+    cout << "未成年，請你好好讀書" >> endl;
+}
